f60cfa290b0ad5a27532d9307f33df4dc69f3a61
jpnavail <96840666+jpnavail@users.noreply.github.com>
2022-11-13 08:47:42 +0100

    Update app.js

--- apps/jpngeneral/app.js
+++ apps/jpngeneral/app.js
@@ -5,269 +5,99 @@
 // -------------   CHARGEMENT FICHIERS  FONTS 
 require("Font8x12").add(Graphics);
 
-var Anton;
-Anton = require("Storage").read("P_Anton.raw");
+var Anton;Anton=require("Storage").read("P_Anton.raw");
 Graphics.prototype.setFontAnton = function(scale) {
   // Actual height 69 (68 - 0)
-  g.setFontCustom(atob(Anton), 46, atob("EiAnGicnJycnJycnEw=="), 78 + (scale << 8) + (1 << 16));
-};
+g.setFontCustom(atob(Anton), 46, atob("EiAnGicnJycnJycnEw=="), 78 + (scale << 8) + (1 << 16)); };
 
-var Anton_Small;
-Anton_Small = require("Storage").read("P_Anton_Small.raw");
+var Anton_Small; Anton_Small=require("Storage").read("P_Anton_Small.raw");
 Graphics.prototype.setFontAntonSmall = function(scale) {
   // Actual height 53 (52 - 0)
-  g.setFontCustom(atob(Anton_Small), 60 + (scale << 8) + (1 << 16));
-};
+  g.setFontCustom(atob(Anton_Small), 60 + (scale << 8) + (1 << 16));};
 
-var couleur = [{
-    "code": "1",
-    "nom": "Noir",
-    "valeur": "0x0000"
-  },
-  {
-    "code": "2",
-    "nom": "Bleu_fonce",
-    "valeur": "0x000F"
-  },
-  {
-    "code": "3",
-    "nom": "Vert_fonce",
-    "valeur": "0x03E0"
-  },
-  {
-    "code": "4",
-    "nom": "Bordeaux",
-    "valeur": "0x7800"
-  },
-  {
-    "code": "5",
-    "nom": "Violet",
-    "valeur": "0x780F"
-  },
-  {
-    "code": "6",
-    "nom": "Olive",
-    "valeur": "0x7BE0"
-  },
-  {
-    "code": "7",
-    "nom": "Gris_clair",
-    "valeur": "0xC618"
-  },
-  {
-    "code": "8",
-    "nom": "Gris_fonce",
-    "valeur": "0x7BEF"
-  },
-  {
-    "code": "9",
-    "nom": "Bleu",
-    "valeur": "0x001F"
-  },
-  {
-    "code": "10",
-    "nom": "Vert",
-    "valeur": "0x07E0"
-  },
-  {
-    "code": "11",
-    "nom": "Rouge",
-    "valeur": "0xF800"
-  },
-  {
-    "code": "12",
-    "nom": "Magenta",
-    "valeur": "0xF81F"
-  },
-  {
-    "code": "13",
-    "nom": "Jaune",
-    "valeur": "0xFFE0"
-  },
-  {
-    "code": "14",
-    "nom": "Blanc",
-    "valeur": "0xFFFF"
-  },
-  {
-    "code": "15",
-    "nom": "Orange",
-    "valeur": "0xFD20"
-  },
-  {
-    "code": "16",
-    "nom": "Vert_jaune",
-    "valeur": "0xAFE5"
-  },
-  {
-    "code": "17",
-    "nom": "Rose",
-    "valeur": "0xF81F"
-  },
-  {
-    "code": "18",
-    "nom": "Cyan_fonce",
-    "valeur": "0x03EF"
-  },
-  {
-    "code": "19",
-    "nom": "Cyan",
-    "valeur": "0x07FF"
-  },
-];
+var couleur=[
+  {"code":"1","nom":"Noir","valeur":"0x0000"},
+  {"code":"2","nom":"Bleu_fonce","valeur":"0x000F"},
+  {"code":"3","nom":"Vert_fonce","valeur":"0x03E0"},
+  {"code":"4","nom":"Bordeaux","valeur":"0x7800"},
+  {"code":"5","nom":"Violet","valeur":"0x780F"},
+  {"code":"6","nom":"Olive","valeur":"0x7BE0"},
+  {"code":"7","nom":"Gris_clair","valeur":"0xC618"},
+  {"code":"8","nom":"Gris_fonce","valeur":"0x7BEF"},
+  {"code":"9","nom":"Bleu","valeur":"0x001F"},
+  {"code":"10","nom":"Vert","valeur":"0x07E0"},
+  {"code":"11","nom":"Rouge","valeur":"0xF800"},
+  {"code":"12","nom":"Magenta","valeur":"0xF81F"},
+  {"code":"13","nom":"Jaune","valeur":"0xFFE0"},
+  {"code":"14","nom":"Blanc","valeur":"0xFFFF"},
+  {"code":"15","nom":"Orange","valeur":"0xFD20"},
+  {"code":"16","nom":"Vert_jaune","valeur":"0xAFE5"},
+  {"code":"17","nom":"Rose","valeur":"0xF81F"},
+  {"code":"18","nom":"Cyan_fonce","valeur":"0x03EF"},
+  {"code":"19","nom":"Cyan","valeur":"0x07FF"},
+   ];
 
 //  Type affichage : Fichier Fond / No ,
 //  xx : 1 HH:MM, 2/ Date  3 :Ligne =>  bg:rectange/No , couleur fg : coul texte
 
-aff_type = [{
-    "bg": "colioure",
-    "bg_l1": "No",
-    "fg_l1": "Bleu_fonce",
-    "bg_l2": "No",
-    "fg_l2": "Bleu_fonce",
-    "bg_l3": "No",
-    "fg_l3": "Blanc"
-  },
-  {
-    "bg": "fd_bg_cal",
-    "bg_l1": "Rouge",
-    "fg_l1": "Blanc",
-    "bg_l2": "No",
-    "fg_l2": "Noir",
-    "bg_l3": "No",
-    "fg_l3": "Noir"
-  },
-  {
-    "bg": "terre",
-    "bg_l1": "No",
-    "fg_l1": "Jaune",
-    "bg_l2": "No",
-    "fg_l2": "Blanc",
-    "bg_l3": "No",
-    "fg_l3": "Blanc"
-  },
-  {
-    "bg": "fd_bg_mult",
-    "bg_l1": "No",
-    "fg_l1": "Noir",
-    "bg_l2": "Noir",
-    "fg_l2": "Jaune",
-    "bg_l3": "Noir",
-    "fg_l3": "Blanc"
-  },
-  {
-    "bg": "montagne",
-    "bg_l1": "No",
-    "fg_l1": "Jaune",
-    "bg_l2": "No",
-    "fg_l2": "Blanc",
-    "bg_l3": "No",
-    "fg_l3": "Noir"
-  },
-  {
-    "bg": "fd_bg_eau",
-    "bg_l1": "No",
-    "fg_l1": "Rouge",
-    "bg_l2": "No",
-    "fg_l2": "Rouge",
-    "bg_l3": "No",
-    "fg_l3": "Rouge"
-  },
-];
+aff_type=[ 
+  { "bg":"colioure","bg_l1":"No","fg_l1":"Bleu_fonce","bg_l2":"No","fg_l2":"Bleu_fonce","bg_l3":"No","fg_l3":"Blanc"},
+  { "bg":"fd_bg_cal","bg_l1":"Rouge","fg_l1":"Blanc","bg_l2":"No","fg_l2":"Noir","bg_l3":"No","fg_l3":"Noir"},
+  { "bg":"terre","bg_l1":"No","fg_l1":"Jaune","bg_l2":"No","fg_l2":"Blanc","bg_l3":"No","fg_l3":"Blanc"},
+  { "bg":"fd_bg_mult","bg_l1":"No","fg_l1":"Noir","bg_l2":"Noir","fg_l2":"Jaune","bg_l3":"Noir","fg_l3":"Blanc"},
+  { "bg":"montagne","bg_l1":"No","fg_l1":"Jaune","bg_l2":"No","fg_l2":"Blanc","bg_l3":"No","fg_l3":"Noir"},
+  { "bg":"fd_bg_eau","bg_l1":"No","fg_l1":"Rouge","bg_l2":"No","fg_l2":"Rouge","bg_l3":"No","fg_l3":"Rouge"},
+  ];
 
 //-----------------------------------------------------------------------
 //                VARIABLES  GENERALES
 //-----------------------------------------------------------------------
-var counterInterval;
-var momoregular;
-var boutton;
-var x = g.getWidth() / 2;
-var y = g.getHeight() / 2;
-var Haut = 24;
-var img = "";
-var fic;
-var T;
-var lit_img = 0;
+var counterInterval; var momoregular;var boutton;
+var x = g.getWidth() / 2; var y = g.getHeight() / 2; var Haut=24;
+var img="";var fic; var T;
+var lit_img=0;
 var timeout;
 //-----------------------------------------------------------------------
 //                FONCTIONS UTILITAIRES
 //-----------------------------------------------------------------------
-function Tsleep(ms) {  var ok = 0;  var debut;  var maintenant;
+function Tsleep(ms) { var ok="0"; var debut;var maintenant;
   console.log("----");
-  debut = Date.now();
-  while (ok == 0) {
-    if (boutton == 1) {ok = 1; boutton = 0; } 
-    else { maintenant = Date.now(); if (maintenant - debut <= ms) ok = 0; else ok = 1;  }
+  debut=Date.now(); while (ok=="0") { 
+  if (boutton ==1 ) {ok="1"; boutton=0;} else{maintenant=Date.now(); if(maintenant-debut <= ms) {ok="0";} else {ok="1";} }
   }
 }
 
-function Sleep(s) { var nb; 
-  if (Math.floor(s) != 0) {
-    for (nb = 1; nb <= Math.floor(s); nb++) Tsleep(1000);
-  }
-  nb = Math.floor(s) - s;
-  console.log(nb);
-  if (nb != 0)  Tsleep(nb);
+function Sleep (s){ var nb;
+  if (Math.floor(s)!=0) { for (nb=1;nb<=Math.floor(s);nb++) {Tsleep(1000);} }
+  nb=Math.floor(s)-s; console.log (nb); if(nb!=0) { Tsleep(nb); }
 }
 
-function Tsleep_Led(ms, led, etat) {
-  var change = 0;
-  var ok = 0;
-  var debut;
-  var maintenant;
-  debut = Date.now();
-  while (ok == 0) {
-    // TEST FIN
-    if (boutton == 1) { ok = 1; boutton = 0; } 
-    else { maintenant = Date.now(); if (maintenant - debut <= ms)  ok = 0; else ok = 1;   }
+function Tsleep_Led(ms,led,etat) {   var change=0; var ok=0; var debut; var maintenant;
+  debut=Date.now();
+  while (ok==0) {
+     // TEST FIN
+     if (boutton ==1 ) { ok=1; boutton=0;}
+     else { maintenant=Date.now(); if(maintenant-debut <=ms) {ok=0;} else {ok=1;}  }
     // BOUCLE + LED
-    if (change == 0) { if (etat == 1) {change = 1; if (led == 1) LED1.write(1);  else LED2.write(1); } } 
-    else { change = 0;    if (led == 1)  LED1.write(0); else  LED2.write(0);     }
+    if (change==0) { if (etat==1) {  change=1; if (led==1) {LED1.write(1);} else { LED2.write(1);}  }}
+    else { change=0; if (led==1) {LED1.write(0);} else { LED2.write(0);} }
 
     Tsleep(500);
   }
 }
-
+  
 //--------------------------------------
 //-------FCT -AFFICHAGES ----------------
-function set_Aff() {
-  g.clearRect(0, 0, 176, 176);
-  Bangle.drawWidgets();
-  g.setFontAlign(0, 0);
-  g.setFont("Vector", 40);
-  g.setColor(0, 0, 0);
-}
+function set_Aff() { g.clearRect(0,0,176,176); Bangle.drawWidgets(); g.setFontAlign(0,0); g.setFont("Vector",40); g.setColor(0,0,0);}
 
-function color_it(c_champ) {
-  var ic;
-  g.setColor(Number("0x000F"));
-  for (ic in couleur) {
-    if (c_champ == couleur[ic].nom) {
-      g.setColor(Number(couleur[ic].valeur));
-      ic = couleur.length;
-    }
-  }
-}
+function color_it(c_champ) { var ic; g.setColor(Number("0x000F"));for (ic in couleur ) {if (c_champ == couleur[ic].nom) 
+ { g.setColor(Number(couleur[ic].valeur));ic=couleur.length;} }}
 
-function lit_fic(i) {
-  console.log(fic = fic_img[i] + ".raw", "Lecture");
-  img = require("Storage").read(fic = fic_img[i] + ".raw");
-  console.log(img.length);
-  lit_img = 1;
-  return (img);
-}
+function lit_fic (i) { console.log(fic=fic_img[i]+".raw","Lecture"); img=require("Storage").read(fic=fic_img[i]+".raw"); console.log(img.length);lit_img=1;return(img); }
 
-function D_ecran(img, x, y) {
-  g.clearRect(0, 0, 176, 176);
-  g.drawImage(atob(img), x, y);
-  Bangle.drawWidgets();
-  console.log("xx");
-}
+function D_ecran(img,x,y) {g.clearRect(0,0,176,176);g.drawImage(atob(img), x, y);Bangle.drawWidgets();console.log("xx");}
 
-function aff_momoregular() {
-  console.log("regular");
-  D_ecran(img, 0, 24, 1);
-}
+function aff_momoregular() {console.log("regular");D_ecran(img,0,24,1);}
 
 //-----------------------------------------------------
 //      Memoire = pack 
@@ -288,13 +118,8 @@ function ressource() {
 //-------------------------------------------------------
 // temperatures pression, altitude 
 //
-var history_T = [];
-var history_P = [];
-var history_A = [];
-var temperature;
-var pression;
-var altitude;
-var data;
+var history_T = []; var history_P = []; var history_A = [];
+var temperature; var pression; var altitude; var data;
 
 function acqui_press() {
   Bangle.getPressure().then(data => {
@@ -304,12 +129,9 @@ function acqui_press() {
 
 //------------------------------
 function suite_press(data) {
-  var temp = data.temperature;
-  var rect_temp = -6.5;
-  var press = data.pressure;
-  var rect_press = 0;
-  var alti = data.altitude;
-  var rect_alti = 62;
+  var temp = data.temperature; var rect_temp = -6.5;
+  var press = data.pressure;  var rect_press = 0;
+  var alti = data.altitude;  var rect_alti = 62;
 
   // moyenne de 5 temperatures
   while (history_T.length > 4) history_T.shift();
@@ -343,13 +165,8 @@ function suite_press(data) {
   altitude = parseInt(altitude) + 0;
 }
 
-var calendar = [];
-var current = [];
-var next = [];
-var event = [];
-var titre;
-var nb_heures;
-var nb_minutes;
+var calendar = []; var current = []; var next = []; var event = [];
+var titre; var nb_heures; var nb_minutes;
 //-----------------------------------------------------------------------------------------
 function updateCalendar() {
   calendar = require("Storage").readJSON("android.calendar.json", true) || [];
@@ -763,9 +580,6 @@ Bangle.setOptions({
 
 acqui_press();
 
-if (!counterInterval)
-  counterInterval = setInterval(aff_principal, rafraichi * 1000);
-
 // rafraichissement en secondes   nb de cycles avant momo
 //var rafraichi=6; var nbaffpr_momo=2; // tous les x/2 momo 
 var rafraichi = 60;
@@ -782,6 +596,9 @@ inter_momo = ((nbaffpr_momo * rafraichi) - moins_tps) * 1000;
 
 if (!momoregular)
   momoregular = setInterval(aff_momoregular, inter_momo);
+
+if (!counterInterval)
+  counterInterval = setInterval(aff_principal, rafraichi * 1000);
 
 //------------------
 ecran = 0;
