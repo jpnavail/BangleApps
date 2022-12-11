70639597c516413043fe9f2cbc5af1eb6cf1e16a
jpnavail <96840666+jpnavail@users.noreply.github.com>
2022-12-11 06:14:41 +0100

    Update app.js

--- apps/jpngeneral/app.js
+++ apps/jpngeneral/app.js
@@ -211,9 +211,10 @@ function remanie_elem (event)
   sortie.journee=event.allDay;
   
   var Rdv = new Date(event.timestamp * 1000);
+  var Maintenant = Date.now();
   sortie.HH=zp(Rdv.getHours())+":"+zp(Rdv.getMinutes());
   //console.log(now,Rdv);
-  sortie.nbj=Math.floor(Rdv.getTime()/d_jour)-Math.floor(now.getTime()/d_jour);
+  sortie.nbj=Math.floor(Rdv.getTime()/d_jour)-Math.floor(Maintenant.getTime()/d_jour);
   if (sortie.nbj==0) 
      {sortie.date="          ";
       sortie.premier=sortie.HH;}
